28510ef4790ec4f024884944d3931b8bb37424ef
BruceAu <bruceau00@gmail.com>
2020-08-28 18:27:54 +0800

    feat: init project

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var j = 1;
+var index = (function () {
+    console.log(j);
+    var map = new Map();
+    map.set(1, 2);
+});
+
+module.exports = index;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const j = 1;
+
+export default ():void => {
+  console.log(j);
+  const map = new Map();
+  map.set(1, 2);
+};
